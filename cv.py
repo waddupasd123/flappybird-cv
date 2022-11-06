4c2febddff800e4acb0b93dd17a0cabd55e7da8b
Andrew Chen <z5259171@ad.unsw.edu.au>
2022-11-06 19:51:36 +1100

    Cleaned up

--- cv.py
+++ cv.py
@@ -28,65 +28,30 @@ def main():
 
         # get an updated image of the game
         screenshot = wincap.get_screenshot()
+        #frame = cv.cvtColor(screenshot, cv.COLOR_RGB2GRAY)
 
-        #pipe1
-        pipe1_img = cv.imread('pipe1_1.png', cv.IMREAD_ANYCOLOR)
-        detect(pipe1_img, screenshot, 0.7)
+        # Game lags with more than 2 objects to detect
+        # Will sometimes not detect because score will cover pipe
+        # Hide score?
 
-        """ pipe_w = pipe1_img.shape[1]
-        pipe_h = pipe1_img.shape[0]
+        # Up pipes
+        pipe1_img = cv.imread('refer/pipe_1.png', cv.IMREAD_ANYCOLOR)
+        upPipes = detect(pipe1_img, screenshot, 0.8, (0, 255, 0))
 
-        #redbird1_img = cv.imread('assets/sprites/redbird-downflap.png')
+        # Down pipes
+        pipe3_img = cv.imread('refer/pipe_3.png', cv.IMREAD_ANYCOLOR)
+        downPipes = detect(pipe3_img, screenshot, 0.8, (255, 0, 0))
 
-        result = cv.matchTemplate(screenshot, pipe1_img, cv.TM_CCOEFF_NORMED)
+        # Bird
+        bird_img = cv.imread('refer\yellowbird-midflap.png', cv.IMREAD_ANYCOLOR)
+        birdLoc = detect(bird_img, screenshot,  0.6, (0, 0, 255))
 
-        #Show matching heatmap
-        #cv.imshow('Result', result)
-        #cv.waitKey()
-
-        #print(result)
-
-        threshold = 0.7
-        locations = np.where(result >= threshold)
-        # Convert to (x,y) positions
-        locations = list(zip(*locations[::-1]))
-        #print(locations)
-
-        # Create list of [x, y, w, h] rectangles
-        rectangles = []
-        for loc in locations:
-            rect = [int(loc[0]), int(loc[1]), pipe_w, pipe_h]
-            rectangles.append(rect)
-            rectangles.append(rect)
-
-        rectangles, weights = cv.groupRectangles(rectangles, 1, 0.5)
-        print(rectangles)
-
-        if len(rectangles):
-            print('Found!')
-
-            pipe_w = pipe1_img.shape[1]
-            pipe_h = pipe1_img.shape[0]
-            line_colour = (0, 255, 0)
-            line_type = cv.LINE_4
-
-            for (x, y, w, h) in rectangles:
-                top_left = (x, y)
-                bottom_right = (x + w, y + h)
-
-                cv.rectangle(screenshot, top_left, bottom_right, line_colour, line_type)
-                #cv.imwrite('result.jpg', screenshot)
-
-            cv.imshow('result.jpg', screenshot)
-
-        else:
-            print('Not found.') """
 
         cv.imshow('result.jpg', screenshot)
 
         # debug the loop rate
-        print('FPS {}'.format(1 / (time() - loop_time)))
-        loop_time = time()
+        #print('FPS {}'.format(1 / (time() - loop_time)))
+        #loop_time = time()
 
 
         if cv.waitKey(1) == ord('q'):
@@ -94,7 +59,7 @@ def main():
             break
 
 
-def detect(image, screenshot, threshold):
+def detect(image, screenshot, threshold, colour):
     result = cv.matchTemplate(screenshot, image, cv.TM_CCOEFF_NORMED)
 
     locations = np.where(result >= threshold)
@@ -114,18 +79,18 @@ def detect(image, screenshot, threshold):
         rectangles.append(rect)
 
     rectangles, weights = cv.groupRectangles(rectangles, 1, 0.5)
-    print(rectangles)
+    #print(rectangles)
 
     if len(rectangles):
-        print('Found!')
-        line_colour = (0, 255, 0)
         line_type = cv.LINE_4
 
         for (x, y, w, h) in rectangles:
             top_left = (x, y)
             bottom_right = (x + w, y + h)
 
-            cv.rectangle(screenshot, top_left, bottom_right, line_colour, line_type)
+            cv.rectangle(screenshot, top_left, bottom_right, colour, line_type)
+    
+    return rectangles
 
 
 if __name__ == '__main__':
